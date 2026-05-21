28ed5b626e46310378d9ec7bb8fbfdb042f60bdf
LukeStenson1 <luke.s.stenson@gmail.com>
2026-05-21 16:04:01 -0600

    Refactor routing to use Layout component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import "@/App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import { Toaster } from "./components/ui/sonner";
+import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Stats from "./pages/Stats";
 import DraftBoard from "./pages/DraftBoard";
@@ -16,20 +17,23 @@ function App() {
     <div className="App">
       <AuthProvider>
         <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/this-week" element={<Navigate to="/" replace />} />
-            <Route path="/lineup" element={<Lineup />} />
-            <Route path="/start-sit" element={<Navigate to="/lineup" replace />} />
-            <Route path="/trades" element={<Trades />} />
-            <Route path="/sleepers-busts" element={<Navigate to="/draft-board" replace />} />
-            <Route path="/rookies" element={<Navigate to="/draft-board" replace />} />
-            <Route path="/draft-board" element={<DraftBoard />} />
-            <Route path="/stats" element={<Stats />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/my-rankings" element={<MyRankings />} />
-          </Routes>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/this-week" element={<Navigate to="/" replace />} />
+              <Route path="/lineup" element={<Lineup />} />
+              <Route path="/start-sit" element={<Navigate to="/lineup" replace />} />
+              <Route path="/trades" element={<Trades />} />
+              <Route path="/sleepers-busts" element={<Navigate to="/draft-board" replace />} />
+              <Route path="/rookies" element={<Navigate to="/draft-board" replace />} />
+              <Route path="/draft-board" element={<DraftBoard />} />
+              <Route path="/stats" element={<Stats />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/my-rankings" element={<Navigate to="/my-rankings" replace />} />
+              <Route path="/my-rankings" element={<MyRankings />} />
+            </Routes>
+          </Layout>
           <Toaster position="top-right" theme="dark" />
         </BrowserRouter>
       </AuthProvider>
