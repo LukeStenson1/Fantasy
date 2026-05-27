65a380d524a17e71e783b216bd8bd697460908c4
LukeStenson1 <luke.s.stenson@gmail.com>
2026-05-27 10:51:04 -0600

    Update mlb_data_service.py

--- backend/mlb_data_service.py
+++ backend/mlb_data_service.py
@@ -165,8 +165,8 @@ def _fetch_mlb_players_sync(seasons_back: int = 3) -> list[dict]:
     """Fetch MLB player stats using pybaseball."""
     mlb_pos_lookup = _fetch_mlb_positions()
     try:
-        from pybaseball import batting_stats
-        from pybaseball import pitching_stats
+        from pybaseball import batting_stats_bref as batting_stats
+        from pybaseball import pitching_stats_bref as pitching_stats
         from pybaseball import cache
         cache.enable()
         import pandas as pd
